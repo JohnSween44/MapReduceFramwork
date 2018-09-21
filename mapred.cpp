1282a8d441e1a790de8beb1892a0cd89c45c1189
JohnSween44 <31758362+JohnSween44@users.noreply.github.com>
2018-09-21 12:14:53 -0400

    Add files via upload

--- mapred.cpp
+++ mapred.cpp
@@ -1,7 +1,15 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <algorithm>
+#include <vector>
+#include <utility>
 
 
-int main(int argc, char* argv[]){
-	
+void vectorizer(std::ifstream * in, std::vector < std::string > * vectorIn);
+
+int main(int argc, char ** argv){
+
 	//Variable initialization
 	int app;
 	int impl;
@@ -32,7 +40,53 @@ int main(int argc, char* argv[]){
 	}else{
 		
 	}
+	std::ifstream readin ; 
+	readin.open(input);
+
+	//Vector of pairs to read into
+	std::vector < std::string > totList;
+
+	//Tokenize  
+	vectorizer(&readin, &totList);
+
+
+	for (int i = 0; i < totList.size(); i++){
+		std::cout << totList[i] << "\n";
+	}
+
+	std::cout << "Total Size: " << totList.size() << "\n";
 
 	return 0;
 }
 
+
+
+/*
+Working as of 9/21/18
+Input: 
+	std::ifstream * in
+		- File input stream, already opened.
+	std::vector <std::string> * vectorIn
+		- External vector that function will read words from file into
+Output:
+	Tokenzied vector of all the words present in file passed in to function.
+*/
+void vectorizers(std::ifstream * in, std::vector < std::string > * vectorIn){
+
+	//Open file input stream and set local vars for input and vector;
+	std::ifstream * mapRead = in;
+	std::vector < std::string > * vec = vectorIn;
+
+	//Variable to hold the word
+	std::string a;
+
+	//Goes through file while has input and adds to passed in array. 
+	while (*in >> a)
+	{	
+		std::transform(a.begin(), a.end(), a.begin(), ::tolower);
+		vec->push_back( a );
+	}
+
+	//std::sort(vec->begin(), vec->end());
+
+}
